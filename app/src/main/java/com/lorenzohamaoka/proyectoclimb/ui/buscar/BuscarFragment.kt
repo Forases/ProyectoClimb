767f18ed70e75fb9728d007b2267ab532728d5c7
Lorenzo <daitaku84@gmail.com>
2020-04-21 17:52:07 +0200

    Revert "borrado método replaceItems"
    This reverts commit 7fff62d24d98a99ce67cfbe6760574c603387da8.

--- app/src/main/java/com/lorenzohamaoka/proyectoclimb/ui/buscar/BuscarFragment.kt
+++ app/src/main/java/com/lorenzohamaoka/proyectoclimb/ui/buscar/BuscarFragment.kt
@@ -1,5 +1,6 @@
 package com.lorenzohamaoka.proyectoclimb.ui.buscar
 
+import android.hardware.SensorManager.getOrientation
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -20,7 +21,7 @@ import java.util.*
 
 class BuscarFragment : Fragment() {
 
-    private lateinit var adapter: MySearchAdapter
+    private lateinit var adapter: MyQuoteAdapter
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -43,7 +44,7 @@ class BuscarFragment : Fragment() {
             }
         })
 
-        adapter = MySearchAdapter()
+        adapter = MyQuoteAdapter()
 
         val mDividerItemDecoration = DividerItemDecoration(
             search_list.context,
@@ -53,7 +54,7 @@ class BuscarFragment : Fragment() {
         search_list.adapter = adapter
     }
 
-    class MySearchAdapter: RecyclerView.Adapter<MySearchAdapter.ViewHolder>() {
+    class MyQuoteAdapter: RecyclerView.Adapter<MyQuoteAdapter.ViewHolder>() {
         private var quoteItems: MutableList<ZonasEscalada> = arrayListOf()
         //Store image and arraylist in Temp Array List we Required it later
         var tempListZonas: MutableList<ZonasEscalada> = arrayListOf()
@@ -70,7 +71,12 @@ class BuscarFragment : Fragment() {
 
             holder.itemView.search_nombre_zona.text = item.nombreZona
             holder.itemView.search_localidad.text = item.localidad
-            holder.itemView.search_distancia.text = "(" + item.distancia + " km)"
+            holder.itemView.search_distancia.text = "(" + item.distancia.toString() + " km)"
+        }
+
+        fun replaceItems(items: MutableList<ZonasEscalada>) {
+            this.quoteItems = items
+            notifyDataSetChanged()
         }
 
         //Function to set data according to Search Keyword in ListView
