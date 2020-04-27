9ead028ff30b3a77036dade21f375a7fcf0c01a1
Lorenzo <daitaku84@gmail.com>
2020-04-27 17:37:24 +0200

    c Chart medio arreglado. Me falta insertar los labels, pero no dejo para el final

--- app/src/main/java/com/lorenzohamaoka/proyectoclimb/ZonasActivity.kt
+++ app/src/main/java/com/lorenzohamaoka/proyectoclimb/ZonasActivity.kt
@@ -2,11 +2,13 @@ package com.lorenzohamaoka.proyectoclimb
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.XAxis.XAxisPosition
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
 import com.lorenzohamaoka.proyectoclimb.MainActivity.Companion.zonaEscalada
 import kotlinx.android.synthetic.main.activity_zonas.*
 import java.util.*
@@ -42,15 +44,34 @@ class ZonasActivity : AppCompatActivity() {
 
 
     private fun setBarChart() {
-        val entries = ArrayList<BarEntry>()
-        entries.add(BarEntry(8f, 0))
-        entries.add(BarEntry(2f, 1))
-        entries.add(BarEntry(5f, 2))
-        entries.add(BarEntry(20f, 3))
-        entries.add(BarEntry(15f, 4))
-        entries.add(BarEntry(19f, 5))
+        val values = ArrayList<BarEntry>()
+        values.add(BarEntry(1f, 0f))
+        values.add(BarEntry(2f, 1f))
+        values.add(BarEntry(3f, 2f))
+        values.add(BarEntry(4f, 3f))
+        values.add(BarEntry(5f, 4f))
+        values.add(BarEntry(6f, 5f))
 
-        val barDataSet = BarDataSet(entries, "Nº de vías por grado de dificultad")
+        val barDataSet = BarDataSet(values, "Nº de vías por grado de dificultad")
+        barDataSet.setDrawIcons(false)
+        barDataSet.setDrawValues(true)
+
+        val startColor1 = resources.getColor(android.R.color.holo_green_light)
+        val startColor2 = resources.getColor(android.R.color.holo_blue_light)
+        val startColor3 = resources.getColor(android.R.color.holo_orange_light)
+        val startColor4 = resources.getColor( android.R.color.holo_green_light)
+        val startColor5 = resources.getColor( android.R.color.holo_orange_dark)
+        val startColor6 = resources.getColor( android.R.color.holo_red_light)
+
+        val colorsArray: MutableList<Int> = ArrayList<Int>()
+        colorsArray.add(startColor1)
+        colorsArray.add(startColor2)
+        colorsArray.add(startColor3)
+        colorsArray.add(startColor4)
+        colorsArray.add(startColor5)
+        colorsArray.add(startColor6)
+
+        barDataSet.colors = colorsArray
 
         val labels = ArrayList<String>()
         labels.add("3-4+")
@@ -59,14 +80,27 @@ class ZonasActivity : AppCompatActivity() {
         labels.add("7a-7c+")
         labels.add("8a-8c+")
         labels.add("9a-9b")
-        val data = BarData(labels, barDataSet)
+
+        val xAxis: XAxis = barChart.xAxis
+        xAxis.position = XAxisPosition.BOTTOM
+        xAxis.setDrawGridLines(false)
+        xAxis.setDrawLabels(true)
+//        xAxis.granularity = 1f // only intervals of 1 day
+
+//        xAxis.labelCount = 7
+
+        barChart.axisLeft.setDrawGridLines(false)
+        barChart.axisRight.setDrawGridLines(false)
+
+        val data = BarData(barDataSet)
         barChart.data = data // set the data and list of lables into chart
 
-        barChart.setDescription(null)  // set the description
+        barChart.description = null  // set the description
 
 //        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS)
-        barDataSet.color = resources.getColor(R.color.colorAccent)
+//        barDataSet.color = resources.getColor(R.color.colorAccent)
 
         barChart.animateY(3000)
+        barChart.legend.isEnabled = false
     }
 }
