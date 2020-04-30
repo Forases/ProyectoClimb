d52f18b600e547d450b42927e742be34d166cfc9
Lorenzo <daitaku84@gmail.com>
2020-04-30 11:49:47 +0200

    arreglado el setOnClickListener de la lista de zonas

--- app/src/main/java/com/lorenzohamaoka/proyectoclimb/ui/lista/ListaZonasFragment.kt
+++ app/src/main/java/com/lorenzohamaoka/proyectoclimb/ui/lista/ListaZonasFragment.kt
@@ -2,16 +2,21 @@ package com.lorenzohamaoka.proyectoclimb.ui.lista
 
 import android.app.PendingIntent.getActivity
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.lorenzohamaoka.proyectoclimb.LoginActivity
+import com.lorenzohamaoka.proyectoclimb.MainActivity
 import com.lorenzohamaoka.proyectoclimb.R
+import com.lorenzohamaoka.proyectoclimb.ZonasActivity
+import dam.lorenzohamaoka.climbingapp.models.ZonasEscalada
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.fragment_lista_zonas.*
 import kotlinx.android.synthetic.main.item_zonas_escalada.view.*
@@ -48,6 +53,8 @@ class ListaZonasFragment : Fragment() {
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
             val item = zonasItems[position]
 
+            holder.bind(item, holder.containerView.context)
+
             // Reference to an image file in Cloud Storage
             val storageReference = item.referenciaPortada
             // Download directly from StorageReference using Glide
@@ -57,6 +64,8 @@ class ListaZonasFragment : Fragment() {
                 .into(holder.itemView.imagen_zona)
             // [END storage_load_with_glide]
 
+
+
             holder.itemView.tv_nombreZona.text = item.nombreZona
         }
 
@@ -64,6 +73,16 @@ class ListaZonasFragment : Fragment() {
 
         inner class ViewHolder(override val containerView: View) :
             RecyclerView.ViewHolder(containerView),
-            LayoutContainer
+            LayoutContainer{
+
+            fun bind(zonaEscalada: ZonasEscalada, context: Context) {
+
+                itemView.setOnClickListener {
+                    MainActivity.zonaEscalada = zonaEscalada
+                    val myIntent = Intent(context, ZonasActivity::class.java)
+                    context.startActivity(myIntent)
+                }
+            }
+        }
     }
 }
